df2124d3c95a95ef296fbc9e7fb8dd6914649fb3
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-21 18:22:36 -0400

    Change the naming of the benchmarks

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,13 +1,12 @@
-#![allow(non_snake_case)]
 #![feature(test)]
 
 extern crate md5;
 extern crate test;
 
-#[bench] fn compute_____1_000(bencher: &mut test::Bencher) { compute(    1_000, bencher); }
-#[bench] fn compute____10_000(bencher: &mut test::Bencher) { compute(   10_000, bencher); }
-#[bench] fn compute___100_000(bencher: &mut test::Bencher) { compute(  100_000, bencher); }
-#[bench] fn compute_1_000_000(bencher: &mut test::Bencher) { compute(1_000_000, bencher); }
+#[bench] fn compute_0001000(bencher: &mut test::Bencher) { compute(    1000, bencher); }
+#[bench] fn compute_0010000(bencher: &mut test::Bencher) { compute(   10000, bencher); }
+#[bench] fn compute_0100000(bencher: &mut test::Bencher) { compute(  100000, bencher); }
+#[bench] fn compute_1000000(bencher: &mut test::Bencher) { compute(1_000000, bencher); }
 
 fn compute(size: usize, bencher: &mut test::Bencher) {
     let data = &vec![0xFFu8; size][..];
