868abf62f9b1aab6617fd3622aadeac7de860742
SethDusek <shibe@openmailbox.org>
2018-01-30 17:06:21 +0500

    Sort traits alphabetically

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ use std::io::{Result, Write};
 use std::ops::{Deref, DerefMut};
 
 /// A digest.
-#[derive(PartialEq, Eq, Copy, Clone)]
+#[derive(Clone, Copy, Eq, PartialEq)]
 pub struct Digest(pub [u8; 16]);
 
 impl Deref for Digest {
