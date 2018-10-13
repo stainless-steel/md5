08b7fbe11ccf80dddfbdcb1000f8c643c3a89684
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-10-13 13:11:53 +0200

    Eliminate a few empty lines

--- src/lib.rs
+++ src/lib.rs
@@ -120,12 +120,9 @@ impl Context {
     pub fn compute(mut self) -> Digest {
         let mut input = [0u32; 16];
         let k = ((self.count[0] >> 3) & 0x3F) as usize;
-
         input[14] = self.count[0];
         input[15] = self.count[1];
-
         consume(&mut self, &PADDING[..(if k < 56 { 56 - k } else { 120 - k })]);
-
         let mut j = 0;
         for i in 0..14 {
             input[i] = ((self.buffer[j + 3] as u32) << 24) |
@@ -135,9 +132,7 @@ impl Context {
             j += 4;
         }
         transform(&mut self.state, &input);
-
         let mut digest = [0u8; 16];
-
         let mut j = 0;
         for i in 0..4 {
             digest[j    ] = ((self.state[i]      ) & 0xFF) as u8;
@@ -146,7 +141,6 @@ impl Context {
             digest[j + 3] = ((self.state[i] >> 24) & 0xFF) as u8;
             j += 4;
         }
-
         Digest(digest)
     }
 }
@@ -189,14 +183,12 @@ pub fn compute<T: AsRef<[u8]>>(data: T) -> Digest {
 fn consume(Context { buffer, count, state }: &mut Context, data: &[u8]) {
     let mut input = [0u32; 16];
     let mut k = ((count[0] >> 3) & 0x3F) as usize;
-
     let length = data.len() as u32;
     count[0] = count[0].wrapping_add(length << 3);
     if count[0] < length << 3 {
         count[1] = count[1].wrapping_add(1);
     }
     count[1] = count[1].wrapping_add(length >> 29);
-
     for &value in data {
         buffer[k] = value;
         k += 1;
@@ -218,14 +210,12 @@ fn consume(Context { buffer, count, state }: &mut Context, data: &[u8]) {
 
 fn transform(state: &mut [u32; 4], input: &[u32; 16]) {
     let (mut a, mut b, mut c, mut d) = (state[0], state[1], state[2], state[3]);
-
     macro_rules! add(
         ($a:expr, $b:expr) => ($a.wrapping_add($b));
     );
     macro_rules! rotate(
         ($x:expr, $n:expr) => (($x << $n) | ($x >> (32 - $n)));
     );
-
     {
         macro_rules! F(
             ($x:expr, $y:expr, $z:expr) => (($x & $y) | (!$x & $z));
@@ -237,12 +227,10 @@ fn transform(state: &mut [u32; 4], input: &[u32; 16]) {
                 $a = add!($a, $b);
             });
         );
-
         const S1: u32 =  7;
         const S2: u32 = 12;
         const S3: u32 = 17;
         const S4: u32 = 22;
-
         T!(a, b, c, d, input[ 0], S1, 3614090360);
         T!(d, a, b, c, input[ 1], S2, 3905402710);
         T!(c, d, a, b, input[ 2], S3,  606105819);
@@ -260,7 +248,6 @@ fn transform(state: &mut [u32; 4], input: &[u32; 16]) {
         T!(c, d, a, b, input[14], S3, 2792965006);
         T!(b, c, d, a, input[15], S4, 1236535329);
     }
-
     {
         macro_rules! F(
             ($x:expr, $y:expr, $z:expr) => (($x & $z) | ($y & !$z));
@@ -272,12 +259,10 @@ fn transform(state: &mut [u32; 4], input: &[u32; 16]) {
                 $a = add!($a, $b);
             });
         );
-
         const S1: u32 =  5;
         const S2: u32 =  9;
         const S3: u32 = 14;
         const S4: u32 = 20;
-
         T!(a, b, c, d, input[ 1], S1, 4129170786);
         T!(d, a, b, c, input[ 6], S2, 3225465664);
         T!(c, d, a, b, input[11], S3,  643717713);
@@ -295,7 +280,6 @@ fn transform(state: &mut [u32; 4], input: &[u32; 16]) {
         T!(c, d, a, b, input[ 7], S3, 1735328473);
         T!(b, c, d, a, input[12], S4, 2368359562);
     }
-
     {
         macro_rules! F(
             ($x:expr, $y:expr, $z:expr) => ($x ^ $y ^ $z);
@@ -307,12 +291,10 @@ fn transform(state: &mut [u32; 4], input: &[u32; 16]) {
                 $a = add!($a, $b);
             });
         );
-
         const S1: u32 =  4;
         const S2: u32 = 11;
         const S3: u32 = 16;
         const S4: u32 = 23;
-
         T!(a, b, c, d, input[ 5], S1, 4294588738);
         T!(d, a, b, c, input[ 8], S2, 2272392833);
         T!(c, d, a, b, input[11], S3, 1839030562);
@@ -330,7 +312,6 @@ fn transform(state: &mut [u32; 4], input: &[u32; 16]) {
         T!(c, d, a, b, input[15], S3,  530742520);
         T!(b, c, d, a, input[ 2], S4, 3299628645);
     }
-
     {
         macro_rules! F(
             ($x:expr, $y:expr, $z:expr) => ($y ^ ($x | !$z));
@@ -342,12 +323,10 @@ fn transform(state: &mut [u32; 4], input: &[u32; 16]) {
                 $a = add!($a, $b);
             });
         );
-
         const S1: u32 =  6;
         const S2: u32 = 10;
         const S3: u32 = 15;
         const S4: u32 = 21;
-
         T!(a, b, c, d, input[ 0], S1, 4096336452);
         T!(d, a, b, c, input[ 7], S2, 1126891415);
         T!(c, d, a, b, input[14], S3, 2878612391);
@@ -365,7 +344,6 @@ fn transform(state: &mut [u32; 4], input: &[u32; 16]) {
         T!(c, d, a, b, input[ 2], S3,  718787259);
         T!(b, c, d, a, input[ 9], S4, 3951481745);
     }
-
     state[0] = add!(state[0], a);
     state[1] = add!(state[1], b);
     state[2] = add!(state[2], c);
@@ -385,7 +363,6 @@ mod tests {
             "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
             "12345678901234567890123456789012345678901234567890123456789012345678901234567890",
         ];
-
         let outputs = [
             "d41d8cd98f00b204e9800998ecf8427e",
             "0cc175b9c0f1b6a831c399e269772661",
@@ -395,7 +372,6 @@ mod tests {
             "d174ab98d277d9f5a5611c2c9f419d9f",
             "57edf4a22be3c955ac49da2e2107b67a",
         ];
-
         for (input, &output) in inputs.iter().zip(outputs.iter()) {
             assert_eq!(format!("{:x}", ::compute(input)), output);
         }
