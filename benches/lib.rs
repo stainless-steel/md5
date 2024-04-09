b63363ed4c770d15b7d6738d35fac77b181e462a
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-04-09 14:54:31 +0200

    Fix the lints

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,13 +1,27 @@
 #![feature(test)]
-#![rustfmt::skip]
 
 extern crate md5;
 extern crate test;
 
-#[bench] fn compute_0001000(bencher: &mut test::Bencher) { compute(   1000, bencher); }
-#[bench] fn compute_0010000(bencher: &mut test::Bencher) { compute(  10000, bencher); }
-#[bench] fn compute_0100000(bencher: &mut test::Bencher) { compute( 100000, bencher); }
-#[bench] fn compute_1000000(bencher: &mut test::Bencher) { compute(1000000, bencher); }
+#[bench]
+fn compute_0001000(bencher: &mut test::Bencher) {
+    compute(1000, bencher);
+}
+
+#[bench]
+fn compute_0010000(bencher: &mut test::Bencher) {
+    compute(10000, bencher);
+}
+
+#[bench]
+fn compute_0100000(bencher: &mut test::Bencher) {
+    compute(100000, bencher);
+}
+
+#[bench]
+fn compute_1000000(bencher: &mut test::Bencher) {
+    compute(1000000, bencher);
+}
 
 fn compute(size: usize, bencher: &mut test::Bencher) {
     let data = &vec![0xffu8; size][..];
